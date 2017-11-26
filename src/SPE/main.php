2e26eb96f6a324b031e11684c09cb2cc2a4ee42b
ItzzHasan <barakhasan2017@gmail.com>
2017-11-27 00:11:38 +0100

    Angefangen :D

---
+++ src/SPE/main.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace SPE;
+
+use pocketmine\plugin\PluginBase;
+
+class main extends PluginBase{
+
+	public function onEnable(){
+		$this->getLogger()->info("§a+");
+	}
+	
+	public function onDisable(){
+		$this->getLogger()->info("§c-");
+		//Bald....
+	}
+}
